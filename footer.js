31a7e23562d2311ec32f9280164086166fe9c7b2
tatugrigalashvili <146704405+tatugrigalashvili@users.noreply.github.com>
2024-09-02 18:41:45 +0400

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 თათუ გრიგალაშვილი</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ header.js
@@ -0,0 +1,15 @@
+function Header(){
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>მთავარი</li>
+          <li>ჩვენ შესახებ</li>
+          <li>კონტაქტი</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
